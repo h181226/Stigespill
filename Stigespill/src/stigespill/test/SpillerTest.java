51a629fc76b6a309f4a9468695770b3d42efa704
Ehrensverd <mail.eskil@gmail.com>
2021-01-29 15:46:54 +0100

    Test Spiller.startTur()

--- Stigespill/src/stigespill/test/SpillerTest.java
+++ Stigespill/src/stigespill/test/SpillerTest.java
@@ -1,13 +1,32 @@
+import stigespill.Brett;
+import stigespill.Spiller;
+import stigespill.Terning;
+
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpillerTest {
-
+    Brett testBrett = new Brett(99);
+    Spiller spiller = new Spiller("Chen", "White");
+    Terning terning = new Terning();
+    Random random = new Random();
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+
+
     }
 
     @org.junit.jupiter.api.Test
     void startTur() {
+        int position = 0;
+
+
+
+
+
+
+
     }
 
     @org.junit.jupiter.api.Test

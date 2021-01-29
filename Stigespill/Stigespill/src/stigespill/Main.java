ad053249e4867d6b66771434d87f6c72c281050e
Simen Gjerde <31205791+h181226@users.noreply.github.com>
2021-01-29 12:43:08 +0100

    Add files via upload

---
+++ Stigespill/Stigespill/src/stigespill/Main.java
@@ -0,0 +1,15 @@
+package stigespill;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Stigespill stigespill = new Stigespill();
+		stigespill.startSpill();
+
+	}
+
+	}
+
+

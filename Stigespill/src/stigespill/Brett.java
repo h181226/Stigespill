078b9c9e46a4926230395de1e51dd6f05a3fdae7
h181226 <h181226@stud.hvl.no>
2021-01-29 13:10:37 +0100

    Oppdatert Brett

--- Stigespill/src/stigespill/Brett.java
+++ Stigespill/src/stigespill/Brett.java
@@ -2,33 +2,15 @@ package stigespill;
 
 public class Brett {
 
+	private Rute[] spillerbrett;
 
-	private int str;
-	private int[] spillerbrett;
-
-	public Brett() {
-		this.str = 100;
-		this.spillerbrett = new int[str];
-		
-		this.spillerbrett[2] = 36;
-		this.spillerbrett[4] = 10;
-		this.spillerbrett[8] = 23;
-		this.spillerbrett[16] = -10;
-		this.spillerbrett[21] = 21;
-		this.spillerbrett[28] = 66;
-		this.spillerbrett[36] = 8;
-		this.spillerbrett[47] = -21;
-		this.spillerbrett[49] = -38;
-		this.spillerbrett[51] = 16;
-		this.spillerbrett[56] = -3;
-		this.spillerbrett[62] = -44;
-		this.spillerbrett[64] = -4;
-		this.spillerbrett[71] = 20;
-		this.spillerbrett[80] = 20;
-		this.spillerbrett[87] = -63;
-		this.spillerbrett[92] = -19;
-		this.spillerbrett[95] = -20;
-		this.spillerbrett[98] = -20;
+	/**
+	 * Oppretter ett brett med en gitt størrelse
+	 * 
+	 * @param Størrelse på brettet
+	 */
+	public Brett(int str) {
+		this.spillerbrett = new Rute[str];
 	}
 	
 }

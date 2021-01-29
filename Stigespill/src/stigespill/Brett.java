2440a7aed4b2987e59ffccb60788b999f1823f8d
h181226 <h181226@stud.hvl.no>
2021-01-29 13:54:08 +0100

    Opprettet brettet med korrekte ruter

--- Stigespill/src/stigespill/Brett.java
+++ Stigespill/src/stigespill/Brett.java
@@ -1,22 +1,50 @@
 package stigespill;
 
+import java.util.Iterator;
+
 public class Brett {
 
-	private Rute[] spillerbrett;
+	private Rute[] spillerBrett;
 
 	/**
 	 * Oppretter ett brett med en gitt størrelse
 	 * 
 	 * @param Størrelse på brettet
 	 */
-	public Brett(int str) {
-		this.spillerbrett = new Rute[str];
+	public Brett(int antallRuter) {
+		this.spillerBrett = new Rute[antallRuter];
 	}
 	
 	/**
-	 * 
+	 * Oppretter spillerbretter med alle rutene inkludert spesialrutene
 	 */
 	public void oppretteBrett() {
+		//Oppretter alle ruter som standard ruter
+		for (int i = 0; i < spillerBrett.length; i++) {
+			spillerBrett[i] =  new Rute(i+1);
+		}
 		
+		//Oppdaterer spesialrutene stiger
+		spillerBrett[2 - 1].setFlyttAntallRuter(38-2);
+		spillerBrett[4 - 1].setFlyttAntallRuter(14-4);
+		spillerBrett[8 - 1].setFlyttAntallRuter(31-8);
+		spillerBrett[21 - 1].setFlyttAntallRuter(42-21);
+		spillerBrett[28 - 1].setFlyttAntallRuter(84-28);
+		spillerBrett[36 - 1].setFlyttAntallRuter(44-36);
+		spillerBrett[51 - 1].setFlyttAntallRuter(67-51);
+		spillerBrett[71 - 1].setFlyttAntallRuter(91-71);
+		spillerBrett[80 - 1].setFlyttAntallRuter(100-80);
+		
+		//Oppdaterer spesialrutene slanger
+		spillerBrett[16 - 1].setFlyttAntallRuter(6-16);
+		spillerBrett[47 - 1].setFlyttAntallRuter(26-47);
+		spillerBrett[49 - 1].setFlyttAntallRuter(11-49);
+		spillerBrett[56 - 1].setFlyttAntallRuter(53-56);
+		spillerBrett[62 - 1].setFlyttAntallRuter(18-62);
+		spillerBrett[64 - 1].setFlyttAntallRuter(60-64);
+		spillerBrett[86 - 1].setFlyttAntallRuter(24-86);
+		spillerBrett[92 - 1].setFlyttAntallRuter(73-92);
+		spillerBrett[95 - 1].setFlyttAntallRuter(75-95);
+		spillerBrett[98 - 1].setFlyttAntallRuter(78-98);
 	}
 }
--- Stigespill/src/stigespill/Rute.java
+++ Stigespill/src/stigespill/Rute.java
@@ -1,5 +1,31 @@
 package stigespill;
 
 public class Rute {
+	
+	private int nummer;
+	private int flyttAntallRuter;
+	
+	public Rute(int nummer) {
+		this.nummer = nummer;
+		flyttAntallRuter = 0;
+	}
+
+	public int getNummer() {
+		return nummer;
+	}
+
+	public void setNummer(int nummer) {
+		this.nummer = nummer;
+	}
+
+	public int getFlyttAntallRuter() {
+		return flyttAntallRuter;
+	}
+
+	public void setFlyttAntallRuter(int flyttAntallRuter) {
+		this.flyttAntallRuter = flyttAntallRuter;
+	}
+	
+	
 
 }
